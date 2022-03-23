012b1ea3d8843b4ec61097cdb978e8120254102d
clxyify <98536033+clxyify@users.noreply.github.com>
2022-03-23 10:19:52 -0400

    Create funcdetect.lua

---
+++ funcdetect.lua
@@ -0,0 +1,9 @@
+local func = "print"
+
+if _G[func] then 
+    _G[func]("detects 'print'")
+  else
+  --how the fuck do you not have 'print'
+end
+
+--to add more keep pasting the code but change 'print' to your function
